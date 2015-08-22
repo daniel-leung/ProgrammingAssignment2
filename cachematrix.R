3a2abb22261b186a88232ce777dd44807d045211
Daniel Leung <daniel.leung2005@gmail.com>
2015-08-22 13:56:37 +0800

    second try

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 y## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix accept a Maxtrix as argument and provide the following functions:
+## set() - store the matrix
+## get() - retrieve the matrix
+## setInverse() - store the inverse matrix provided. Note this function does not calculate the inverse of the matrix
+## getInverser() - get the stored inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -20,7 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve accept an object that is of type makeCacheMatrix and provide the following functions:
+## check if the inverse matrix already calculated and stored in cache by using the getIverse() function
+## of makeCachMatrix.
+## If it does not exist, calculate the inverse matrix and store in cached using setInverse() function
+## of makeCacheMatrixmt
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
